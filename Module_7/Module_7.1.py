308510df8a12aa845ce5e56c05ad9b07f2b28730
Aakash <aakashb@metropolia.fi>
2025-09-21 22:44:14 +0300

    Fourth commit,amendment:  the question specified 12 as winter and 3 months for 1 season.

--- Module_7/Module_7.1.py
+++ Module_7/Module_7.1.py
@@ -1,14 +1,21 @@
-season=('Winter','Spring','Summer','Autumn',)
+season=('Winter','Spring','Summer','Autumn')
 month=int(input("enter the month: "))
 
-if month>=1 and month<4:
-    print(season[0])
-elif month>=4 and month<7:
-    print((season[1]))
-elif month>=7 and month<10:
-    print(season[2])
-elif month>=10 and month<13:
-    print(season[3])
-else:
-    print("That's not a month")
+if month==12 or 1 or 2:
+    print(f'{month} is {season[0]}')
+elif month==3 or 4 or 5:
+    print(f'{month} is {season[1]}')
+elif month==6 or 7 or 8:
+    print(f'{month} is {season[2]}')
+elif month==9 or 10 or 11:
+    print(f'{month} is {season[3]}')
+
+
+
+
+
+
+
+
+
 
